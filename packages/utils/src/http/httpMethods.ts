ee61cadb4e3998cbf6651c2a3f64924328f2848d
ek68794998 <1350074+ek68794998@users.noreply.github.com>
2026-08-04 09:45:02 -0700

    Improve HTTP constants and helpers

--- packages/utils/src/http/httpMethods.ts
+++ packages/utils/src/http/httpMethods.ts
@@ -9,12 +9,29 @@ export type HttpMethod =
 	| "PUT"
 	| "TRACE";
 
-export const Connect: HttpMethod = "CONNECT";
-export const Delete: HttpMethod = "DELETE";
-export const Get: HttpMethod = "GET";
-export const Head: HttpMethod = "HEAD";
-export const Options: HttpMethod = "OPTIONS";
-export const Patch: HttpMethod = "PATCH";
-export const Post: HttpMethod = "POST";
-export const Put: HttpMethod = "PUT";
-export const Trace: HttpMethod = "TRACE";
+export const Connect = "CONNECT" as const satisfies HttpMethod;
+export const Delete = "DELETE" as const satisfies HttpMethod;
+export const Get = "GET" as const satisfies HttpMethod;
+export const Head = "HEAD" as const satisfies HttpMethod;
+export const Options = "OPTIONS" as const satisfies HttpMethod;
+export const Patch = "PATCH" as const satisfies HttpMethod;
+export const Post = "POST" as const satisfies HttpMethod;
+export const Put = "PUT" as const satisfies HttpMethod;
+export const Trace = "TRACE" as const satisfies HttpMethod;
+
+const httpMethods = new Set<HttpMethod>([
+	Connect,
+	Delete,
+	Get,
+	Head,
+	Options,
+	Patch,
+	Post,
+	Put,
+	Trace,
+]);
+
+export const isHttpMethod = (obj: unknown): obj is HttpMethod => {
+	const possibleHttpMethod = obj as HttpMethod;
+	return httpMethods.has(possibleHttpMethod);
+};
